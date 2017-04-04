d5b93aa150d870ea226f02c6714fe471d92685cd
Tianle HOU <thou@e3r5p1.42.fr>
2017-04-04 18:02:38 +0200

    initial

---
+++ includes/ft_ls.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thou <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/04 17:38:04 by thou              #+#    #+#             */
+/*   Updated: 2017/04/04 18:00:25 by thou             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include "libft.h"
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <pwd.h>
+# include <uuid/uuid.h>
+# include <grp.h>
+# include <sys/xattr.h>
+# include <time.h>
+# include <stdio.h>
+# include <string.h>
+
+#endif
